200f6eed21741231f6e50fb439b6fd4bd109e51d
Elshan <elshanaliev2009@gmail.com>
2024-01-04 19:55:49 +0500

    new templates

--- akfa/main/urls.py
+++ akfa/main/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+    path('portfolio/', portfolio, name='portfolio'),
+    path('product/', product, name='product'),
 ]
--- akfa/main/views.py
+++ akfa/main/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+
 # Create your views here.
+
 def home(request):
     return render(request, 'home.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def product(request):
+    return render(request, 'product.html')
 
 
 
